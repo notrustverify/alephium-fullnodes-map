f686eb382e02e2bda5ecee3a56b4c56d32f3d705
cgi-bin <s@ono.re>
2024-05-25 14:09:17 +0200

    rm: ip from api

--- api/main.go
+++ api/main.go
@@ -24,15 +24,12 @@ type FullnodeDb struct {
 	CliqueId          string `gorm:"unique"`
 	BrokerId          uint
 	GroupNumPerBroker uint
-	Ip                string
-	Port              uint
 	ClientVersion     string
 	IsSynced          bool
 	Hostname          string
 	City              string
 	Region            string
 	Country           string
-	Location          string
 	Org               string
 	Postal            string
 	Timezone          string
